439096d6d7836c8730c79ee823b68a26e58adbff
Avner <aluvton@gmail.com>
2025-05-24 03:26:40 +0300

    added firebase

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
 const AppShell = () => {
   return (
     <div>
       <Header />
-      <main>blabla</main>
+      <main>
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyChrVS_AJfErLOBbVhAd7tRUEpVqPZuftI",
+  authDomain: "findme-36f0d.firebaseapp.com",
+  projectId: "findme-36f0d",
+  storageBucket: "findme-36f0d.firebasestorage.app",
+  messagingSenderId: "478217530806",
+  appId: "1:478217530806:web:67c827a2799dd91abca517",
+  measurementId: "G-QSJGFCQCZK",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
